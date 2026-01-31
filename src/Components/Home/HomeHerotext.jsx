31a661cbebb50b73e3027f2bc6be6592721ce5ef
PiyushBytes <dawnpiyush384@gmail.com>
2026-01-31 21:43:31 +0530

    Fixed the Styling of the Home page

--- src/Components/Home/HomeHerotext.jsx
+++ src/Components/Home/HomeHerotext.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Video from "./Video";
 
 const HomeHerotext = () => {
   return (
-    <div>HomeHerotext</div>
-  )
-}
+    <div className="font-[font1] pt-5 text-center">
+      <div className="text-[9.5vw] uppercase leading-[8vw]">L'étincelle</div>
+      <div className="text-[9.5vw] uppercase leading-[8vw] flex items-center">
+        qui
+        <div className="h-[10vw] w-[10vw]">
+          <Video />
+        </div>
+        génère
+      </div>
+      <div className="text-[9.5vw] uppercase leading-[8vw]">la créativité</div>
+    </div>
+  );
+};
 
-export default HomeHerotext
+export default HomeHerotext;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Video from '../Components/Home/Video'
+import HomeHerotext from '../Components/Home/HomeHerotext'
+import HomeBottomText from '../Components/Home/HomeBottomText'
 
 const Home = () => {
   return (
     <div>
-      <div className='h-screen w-screen bg-red-700' >
+      <div className='h-screen w-screen fixed ' >
         <Video/>
+      </div>
+      <div className='h-screen w-screen relative flex flex-col'>
+        <HomeHerotext/>
+        <HomeBottomText/>
 
       </div>
     </div>
